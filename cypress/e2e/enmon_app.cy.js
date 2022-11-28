1a4889bf0c300ef1de19f2d1ac20fb7b038e9a0c
opoll <o.pollach@gmail.com>
2022-11-28 17:49:18 +0100

    hide password

--- cypress/e2e/enmon_app.cy.js
+++ cypress/e2e/enmon_app.cy.js
@@ -1,6 +1,6 @@
 describe('Enmon App', () => {
   it('logs in Enmon app', () => {
     cy.visit('https://pasport.dev.enmon.tech/')
-      cy.get("#authForm_locationApiKey").type("f75cb865ded1c60e{enter}")
+      cy.get("#authForm_locationApiKey").type("password{enter}")
   })
 })
